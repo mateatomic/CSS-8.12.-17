349931b3f3755e7653890e6cc49facbb45314e49
mateatomic <31723042+mateatomic@users.noreply.github.com>
2017-12-08 21:13:10 +0100

    Add files via upload

---
+++ 8.12/css/7/new 1.h
@@ -0,0 +1,11 @@
+body {
+	background-image: url("https://images3.memedroid.com/images/UPLOADED354/570283c779c3c.jpeg");
+}
+
+h1 {
+	background-image: url("https://images3.memedroid.com/images/UPLOADED354/570283c779c3c.jpeg");
+}
+
+p {
+	background-image: url("https://images3.memedroid.com/images/UPLOADED354/570283c779c3c.jpeg");
+}
